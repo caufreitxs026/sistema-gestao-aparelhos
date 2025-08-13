b1742a3a63b4b0ba50125286aef1164076ca3551
Cauã Freitas <cauafreitas026@gmail.com>
2025-08-13 14:14:12 -0300

    Versão 2.3

--- pages/2_Colaboradores.py
+++ pages/2_Colaboradores.py
@@ -86,8 +86,8 @@ with st.sidebar:
             <a href="https://github.com/caufreitxs026" target="_blank" title="GitHub">
                 <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/github.svg">
             </a>
-            <a href="https://instagram.com/Caufreitxs" target="_blank" title="Instagram">
-                <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/instagram.svg">
+            <a href="https://linkedin.com/in/cauafreitas" target="_blank" title="LinkedIn">
+                <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/linkedin.svg">
             </a>
         </div>
         """,
@@ -207,4 +207,5 @@ with col2:
                     if atualizar_colaborador(col_id, novo_codigo, novo_nome, novo_gmail, novo_setor_id):
                         st.toast(f"Colaborador '{novo_nome}' atualizado!", icon="✅")
             st.rerun()
+
 
