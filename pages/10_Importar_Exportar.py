187463150173a38024e139bd8bca213a56be9313
Cauã Freitas <cauafreitas026@gmail.com>
2025-08-13 14:13:23 -0300

    Versão 2.3

--- pages/10_Importar_Exportar.py
+++ pages/10_Importar_Exportar.py
@@ -86,8 +86,8 @@ with st.sidebar:
             <a href="https://github.com/caufreitxs026" target="_blank" title="GitHub">
                 <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/github.svg">
             </a>
-            <a href="https://instagram.com/Caufreitxs" target="_blank" title="Instagram">
-                <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/instagram.svg">
+            <a href="https://linkedin.com/in/cauafreitas" target="_blank" title="LinkedIn">
+                <img src="https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/svgs/brands/linkedin.svg">
             </a>
         </div>
         """,
@@ -378,5 +378,6 @@ elif tabela_selecionada == "Contas Gmail":
 
         except Exception as e:
             st.error(f"Ocorreu um erro ao ler o ficheiro: {e}")
+
 
 
